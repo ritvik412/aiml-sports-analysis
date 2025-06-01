c44136a08c38479a05119ab0ef5697716abb2a57
Ritvik <125459023+ritvik412@users.noreply.github.com>
2025-06-01 18:11:20 +0530

    yolo_inference.py

--- yolo_inference.py
+++ yolo_inference.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8x') # x - extra large for best accuracy (more parameters)
 
-results = model.predict(r'# path to your input video ',save = True)
+results = model.predict(r'C:\Users\kingr\football_project\input_videos\08fd33_4.mp4',save = True) #includes path to the sample input video
 # [save = true] basically writes annotated video to disk 
 
 print(results[0]) #first frame
